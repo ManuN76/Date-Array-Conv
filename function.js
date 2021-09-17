11c9b0a53a5d5de6e4ef6a32beee4d5ed31032c5
ManuN76 <manu.n2@free.fr>
2021-09-18 01:25:46 +0200

    update with options parameters

--- function.js
+++ function.js
@@ -12,8 +12,52 @@
  * - for a conversion of a Date-Time column, use the 'short' format
  *
  */
+window.function = function (
+  array,
+  arrayformat,
+  lang,
+  weekday,
+  year,
+  month,
+  day,
+  hour,
+  minute,
+  second,
+  timeZoneName,
+  era
+) {
+  if (array === undefined) return;
 
-window.function = function (array, arrayformat) {
+  let lg = "";
+  if (lang === undefined || lang.value == "") {
+    lg = navigator.language;
+  } else lg = lang.value;
+
+  weekday = weekday.value ?? "";
+  year = year.value ?? "";
+  month = month.value ?? "";
+  day = day.value ?? "";
+  hour = hour.value ?? "";
+  minute = minute.value ?? "";
+  second = second.value ?? "";
+  timeZoneName = timeZoneName.value ?? "";
+  era = era.value ?? "";
+
+  let ops = new Map();
+  if (weekday) ops.set("weekday", weekday);
+  if (year) ops.set("year", year);
+  if (month) ops.set("month", month);
+  if (day) ops.set("day", day);
+  if (hour) ops.set("hour", hour);
+  if (minute) ops.set("minute", minute);
+  if (second) ops.set("second", second);
+  if (timeZoneName) ops.set("timeZoneName", timeZoneName);
+  if (era) ops.set("ear", era);
+
+  const obj = Object.fromEntries(ops);
+
+  //let x = glide2Date(date.value, "short");
+
   let arrDate = array.value;
 
   glideDateTimeFormat = "short";
@@ -26,7 +70,8 @@ window.function = function (array, arrayformat) {
   if (arrDate != undefined) {
     arrDate = arrDate.map((dt) => {
       let x = glide2Date(dt, glideDateTimeFormat);
-      let conv = x.getDate() + "/" + (x.getMonth() + 1) + "/" + x.getFullYear();
+      //let conv = x.getDate() + "/" + (x.getMonth() + 1) + "/" + x.getFullYear();
+      let conv = x.toLocaleString(lg, obj);
       //arrayConv.push(conv);
       return conv;
     });
